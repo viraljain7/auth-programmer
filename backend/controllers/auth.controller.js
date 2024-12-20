c72995a8ac3bf7590492f633f5ea8d383acf18a2
viral jain <veerljain1234@gmail.com>
2024-12-20 17:19:14 +0530

    feat: add login controller

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -84,12 +84,21 @@ const login = async (req, res) => {
         if (!user) {
             return res.status(400).json({ success: false, error: 'Invalid username or password' });
         }
-        const isMatch = await bcrypt.compare(password, user.password);
-        if (!isMatch) {
-            return res.status(400).json({ error: 'Invalid username or password' });
+        const isPasswordValid = await bcrypt.compare(password, user.password);
+        if (!isPasswordValid) {
+            return res.status(400).json({ success: false, error: 'Invalid username or password' });
         }
-        const token = jwt.sign({ userId: user._id }, 'your_jwt_secret', { expiresIn: '1h' });
-        res.status(200).json({ token });
+        generateTokenAndSetToken(res, user._id);
+        user.lastLogin = Date.now();
+        await user.save();
+        res.status(200).json({
+            success: true,
+            message: "Login successfully",
+            user: {
+                ...user._doc,
+                password: undefined,
+            },
+        });
     } catch (error) {
         res.status(500).json({ success: false, error: 'Internal server error' });
     }
